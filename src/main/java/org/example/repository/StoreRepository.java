5eb5cae90790eb66b97f01e3ad5255848e9ee4e9
brunaPrauchner <bruna.prauchner@acad.pucrs.br>
2024-04-12 14:40:26 -0700

    added update routes for store

--- src/main/java/org/example/repository/StoreRepository.java
+++ src/main/java/org/example/repository/StoreRepository.java
@@ -4,7 +4,12 @@ import org.example.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @SuppressWarnings({"checkstyle:MissingJavadocType", "checkstyle:Indentation"})
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Optional<List<Store>> findStoreByStoreName(String storeName);
+
 }
